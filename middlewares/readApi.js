12d4638ea15b1bd4cc5c1c05f481813187f0c090
vtrtga <vitor.ypvalim@gmail.com>
2022-08-26 14:01:59 -0400

    iniciando projeto

---
+++ middlewares/readApi.js
@@ -0,0 +1,14 @@
+const fs = require('fs').promises;
+
+const readApi = async () => {
+    const path = '/src/talker.json';
+    try {
+        const contentFile = await fs.readFile(path, 'utf-8');
+
+        return JSON.parse(contentFile);
+    } catch (err) {
+        console.log('Não foi possivel ler', err);
+    }
+};
+
+module.exports = readApi;
